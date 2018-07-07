8dfd0c75fd99d094fc2bceb1820a4472d068ac08
zhoushuai <dreamcoffeezs@163.com>
2018-07-07 16:19:10 +0800

    Initial Commit

---
+++ PrivacyAlbum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrivacyAlbum
+//
+//  Created by MOKA_MBP on 2018/7/7.
+//  Copyright © 2018年 weining. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
